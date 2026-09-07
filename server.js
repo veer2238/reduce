d1a5b58b67b83f0113c168701613f42da658414c
“veer2238” <veer2238rajput@gmail.com>
2026-09-07 09:29:42 +0530

    index

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import sharp from "sharp";
 import cors from "cors";
+import cron from "node-cron";
 
 const app = express();
 
@@ -36,6 +37,33 @@ app.post("/api/compress", upload.single("image"), async (req, res) => {
 
     res.status(500).json({
       message: "Image compression failed",
+    });
+  }
+});
+
+
+
+
+
+app.get("/api/birthday/send", async (req, res) => {
+  try {
+    console.log("Birthday cron started");
+
+    // 1. Get today's date
+    // 2. Find students whose birthday is today
+    // 3. Send email
+
+    res.status(200).json({
+      success: true,
+      message: "Birthday check completed"
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: error.message
     });
   }
 });
